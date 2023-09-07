6f30a956e18be2e024864d25ae5911d6e8dd9df3
engxamza <enghamzaomarr@gmail.com>
2023-09-07 12:23:02 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-
 /**
  * main - this is the main function
- *
- * Return: always 0
+ *  Return: always 0
  */
-
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
+  puts("\"Programming is like building a multilingual puzzle");
         return (0);
 }	
