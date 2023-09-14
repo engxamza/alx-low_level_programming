b0f8f09d7c11bf4d1109e285240fe36159f9a0d1
engxamza <enghamzaomarr@gmail.com>
2023-09-14 09:06:40 -0700

    task2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,34 +1,13 @@
 #include "main.h"
-
 /**
- * print_triangle - check for a digit
- * @size : integer type
- * Return:void
+ * mul - returns the product of its parameters
+ * @a: int type number
+ * @b: int type number
+ * Return:0
  */
 
-void print_triangle(int size)
+
+int mul(int a, int b)
 {
-
-	int i = 1, ii;
-
-	while (i <= size && size > 0)
-	{
-		ii = 0;
-		while (ii < size - i)
-		{
-			_putchar(' ');
-			ii++;
-		}
-		ii = 0;
-		while (ii < i)
-		{
-			_putchar('#');
-			ii++;
-		}
-
-		_putchar('\n');
-		i++;
-	}
-	if (i == 1)
-		_putchar('\n');
+return (a * b);
 }
